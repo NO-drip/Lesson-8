da06acecd492fb6bb518a6e52447bd25fc5251f3
No-drip <aneeshthosam@gmail.com>
2026-06-29 11:10:04 +0100

    Number divisibility project

---
+++ Number divisibility.py
@@ -0,0 +1,5 @@
+Numerator = int (input("Enter the numerator:"))
+Denominator = int(input("Enter the denominator."))
+if Numerator%Denominator == 0:
+    print ("These 2 numbers are divisible")
+else: print ("These 2 numbers are not perfectly divisible.")

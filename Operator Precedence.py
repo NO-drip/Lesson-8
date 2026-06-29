547b392a002701656106008c73925909bee26209
No-drip <aneeshthosam@gmail.com>
2026-06-29 10:56:47 +0100

    Operator precedence project

---
+++ Operator Precedence.py
@@ -0,0 +1,12 @@
+v = 98
+w = 65
+x = 13
+y = 37
+z = (v+w)*x/y
+print ("The value of z is:",z)
+
+name = input("Enter a name:")
+age = int (input ("Enter a certain age:"))
+if name == "John" and age >= 2 :
+    print ("Welcome to John's server.")
+else: print ("Goodbye")

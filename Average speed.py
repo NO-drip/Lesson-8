c571ef6f88ebed7cf741e4a9cfc862308a839700
No-drip <aneeshthosam@gmail.com>
2026-06-29 11:20:06 +0100

    Average speed project

---
+++ Average speed.py
@@ -0,0 +1,8 @@
+a = int (input("Enter a number:"))
+b = int (input("Enter your second number:"))
+c = int (input("Enter your third number:"))
+average = (a + b + c) / 3
+if average > a and b and c:
+    print ("The average is greater than a, b and c.")
+else: print ("The average is not greater than a, b and c.") 
+
